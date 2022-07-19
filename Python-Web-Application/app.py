343815e087ed2066bbf4a4aa4b46f72b011df712
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2022-07-19 10:48:55 +0530

    Browsing directory path (PC).

--- Python-Web-Application/app.py
+++ Python-Web-Application/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
 app = Flask(__name__)
+app.secret_key = "1234"
 
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	dir_path = None
+	if 'show_path' in session and session['show_path']:
+		dir_path = session['dir_path']
+		session['show_path'] = False
+	return render_template("index.html", dir_path=dir_path)
+
+
+@app.route("/browse")
+def browse():
+	root = Tk()
+	root.title("Backup System")
+	root.geometry("270x0")
+	root.iconphoto(False, ImageTk.PhotoImage(Image.open('static\\logo\\logo.ico')))
+
+	dir_path = filedialog.askdirectory().strip()
+	if len(dir_path) != 0:
+		session['dir_path'] = dir_path
+		session['show_path'] = True
+	
+	root.destroy()
+	root.mainloop()
+	return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
