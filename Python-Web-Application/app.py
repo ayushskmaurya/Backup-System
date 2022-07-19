91013f74f69fdb9301e135ae0fdbaf30d48b700a
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2022-07-19 16:35:20 +0530

    Listing all files and folders of directory.

--- Python-Web-Application/app.py
+++ Python-Web-Application/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, jsonify
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import os
 
 app = Flask(__name__)
 app.secret_key = "1234"
+
+
+# Listing all the files and folders of the selected directory.
+def pc_files(abs_path, path, pc_dir_list):
+	files = os.listdir(abs_path)
+	for file in files:
+		abs_file_path = os.path.join(abs_path, file)
+		file_path = os.path.join(path, file)
+		isDir, last_modified = None, None
+
+		if os.path.isdir(abs_file_path):
+			isDir = True
+			pc_files(abs_file_path, file_path, pc_dir_list)
+		else:
+			isDir = False
+			last_modified = os.path.getmtime(abs_file_path)
+
+		pc_dir_list[file_path.replace("\\", "/")] = {'isDir': isDir, 'last_modified': last_modified}
 
 
 @app.route("/")
@@ -31,6 +50,13 @@ def browse():
 	root.destroy()
 	root.mainloop()
 	return redirect(url_for('home'))
+
+
+@app.route("/get_pc_dir_list")
+def get_pc_dir_list():
+	pc_dir_list = {}
+	pc_files(session['dir_path'], "", pc_dir_list)
+	return jsonify(pc_dir_list)
 
 
 if __name__ == '__main__':
