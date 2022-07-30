37e2b98a07441b23c8cedd2ea196b4ef3044bc5d
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2022-07-30 20:35:29 +0530

    Fixed relative path issue.

--- Android-Application/app/src/main/java/com/mayush/backupsystem/BackupActivity.java
+++ Android-Application/app/src/main/java/com/mayush/backupsystem/BackupActivity.java
@@ -25,6 +25,11 @@ public class BackupActivity extends AppCompatActivity {
 	private static final String TAG = "AppLogBackupActivity";
 	private static final String ENTERED_DIR_PATH = "com.mayush.backupsystem.enteredDirPath";
 	private File dir;
+	private TextView stepInfoView;
+	private String stepInfo;
+	private JSONObject pcDirList;
+	private HashMap<String, JSONObject> androidDirList;
+	private JSONArray filesToKeep, filesToBackup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +42,13 @@ public class BackupActivity extends AppCompatActivity {
 		TextView enteredDirPathView = findViewById(R.id.enteredDirPath_activityBackup);
 		enteredDirPathView.setText(enteredDirPath);
 
-		TextView stepInfoView = findViewById(R.id.stepInfo_activityBackup);
-		getPcDirList(stepInfoView);
+		stepInfoView = findViewById(R.id.stepInfo_activityBackup);
+		getPcDirList();
 	}
 
 	// Retrieving list of all files & folders of PC.
-	private void getPcDirList(TextView stepInfoView) {
-		String stepInfo = "Retrieving list of all files & folders of PC...";
+	private void getPcDirList() {
+		stepInfo = "Retrieving list of all files & folders of PC...";
 		stepInfoView.setText(stepInfo);
 
 		Volley.newRequestQueue(this).add(
@@ -52,7 +57,8 @@ public class BackupActivity extends AppCompatActivity {
 						Base.getBaseUrl() + "/get_pc_dir_list",
 						response -> {
 							try {
-								getAndroidDirList(stepInfoView, new JSONObject(response));
+								pcDirList = new JSONObject(response);
+								getAndroidDirList();
 							} catch (JSONException e) {
 								Log.e(TAG, e.toString());
 							}
@@ -63,14 +69,14 @@ public class BackupActivity extends AppCompatActivity {
 	}
 
 	// Retrieving list of all files & folders of Android.
-	private void getAndroidDirList(TextView stepInfoView, JSONObject pcDirList) {
-		String stepInfo = "Retrieving list of all files & folders of Android...";
+	private void getAndroidDirList() {
+		stepInfo = "Retrieving list of all files & folders of Android...";
 		stepInfoView.setText(stepInfo);
 
 		if(dir.exists() && dir.isDirectory()) {
-			HashMap<String, JSONObject> androidDirList = new HashMap<>();
-			android_files(dir, "", androidDirList);
-			compareList(pcDirList, androidDirList, stepInfoView);
+			androidDirList = new HashMap<>();
+			android_files(dir, "");
+			compareList();
 		}
 		else {
 			Toast.makeText(this, "Please enter valid directory path", Toast.LENGTH_SHORT).show();
@@ -79,15 +85,16 @@ public class BackupActivity extends AppCompatActivity {
 	}
 
 	// Listing all the files and folders of the entered directory path.
-	private void android_files(File dir, String path, HashMap<String, JSONObject> androidDirList) {
+	private void android_files(File dir, String path) {
 		File[] files = dir.listFiles();
 		for(File file : files) {
+			String filePath = (new File(path, file.getName())).toString();
 			boolean isDir;
 			String lastModified = null;
 
 			if(file.isDirectory()) {
 				isDir = true;
-				android_files(file, file.getName(), androidDirList);
+				android_files(file, filePath);
 			}
 			else {
 				isDir = false;
@@ -98,7 +105,7 @@ public class BackupActivity extends AppCompatActivity {
 			try {
 				fileInfo.put("isDir", isDir);
 				fileInfo.put("lastModified", lastModified);
-				androidDirList.put((new File(path, file.getName())).toString(), fileInfo);
+				androidDirList.put(filePath, fileInfo);
 			} catch (JSONException e) {
 				Log.e(TAG, e.toString());
 			}
@@ -106,12 +113,12 @@ public class BackupActivity extends AppCompatActivity {
 	}
 
 	// Comparing the retrieved list of PC with that of Android's.
-	private void compareList(JSONObject pcDirList, HashMap<String, JSONObject> androidDirList, TextView stepInfoView) {
-		String stepInfo = "Comparing the retrieved list of PC with that of Android's...";
+	private void compareList() {
+		stepInfo = "Comparing the retrieved list of PC with that of Android's...";
 		stepInfoView.setText(stepInfo);
 
-		JSONArray filesToKeep = new JSONArray();
-		JSONArray filesToBackup = new JSONArray();
+		filesToKeep = new JSONArray();
+		filesToBackup = new JSONArray();
 		for(Map.Entry<String, JSONObject> androidFile : androidDirList.entrySet()) {
 			String androidFileName = androidFile.getKey();
 
@@ -132,12 +139,12 @@ public class BackupActivity extends AppCompatActivity {
 			}
 		}
 
-		delete_files(pcDirList, filesToKeep, filesToBackup, stepInfoView);
+		delete_files();
 	}
 
 	// Deleting unwanted files.
-	private void delete_files(JSONObject pcDirList, JSONArray filesToKeep, JSONArray filesToBackup, TextView stepInfoView) {
-		String stepInfo = "Deleting unwanted files...";
+	private void delete_files() {
+		stepInfo = "Deleting unwanted files...";
 		stepInfoView.setText(stepInfo);
 
 		Volley.newRequestQueue(this).add(
